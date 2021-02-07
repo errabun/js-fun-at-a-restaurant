e946aa0367a0b436db9fc6b248eff2a57c907d80
Eric Rabun <errabun@gmail.com>
2021-02-06 18:24:29 -0700

    add order.js & test/order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(orderNum, deliveryArray) {
+  if (deliveryArray.length < 3) {
+    return deliveryArray.push(orderNum);
+  } else {
+    return deliveryArray;
+  }
+}
+
+function refundOrder(orderNum, deliveryArray) {
+  deliveryArray.splice(orderNum - 1, 1);
+  return deliveryArray;
+}
+
+function listItems(deliveryArray) {
+  var items = [];
+  for (var i = 0; i < deliveryArray.length; i++) {
+    items.push(deliveryArray[i].item);
+  }
+  return items.join(', ');
+}
+
+// function searchOrder(deliveryArray, menuItem) {
+//   for (var i = 0; i < deliveryArray.length; i++) {
+//     if (deliveryArray[i].item === menuItem) {
+//     }
+//   }
+function searchOrder(deliveryArray, menuItem) {
+  for (var i = 0; i < deliveryArray.length; i++) {
+    if (deliveryArray[i].item === menuItem) {
+      return true;
+    } else continue;
+  }  return false;
+}
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder,
 }
