e2c0d0859c8076e304cd9e5174de95b9d41a761d
Eric Rabun <errabun@gmail.com>
2021-02-06 18:16:33 -0700

    add meals test $ js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`;
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
+}
+
+function addIngredients(food, ingredients) {
+  if (ingredients.includes(food)) {
+    return ingredients;
+  } else {
+    ingredients.push(food);
+  }
+  return ingredients;
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(discountPrice) {
+  return discountPrice*.9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
