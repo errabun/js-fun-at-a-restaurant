1eeb4a9b507b973cfaa7f1a4de65f72e05f38a72
Eric Rabun <errabun@gmail.com>
2021-02-07 10:13:05 -0700

    Refactor; add pseudocode

--- src/meal.js
+++ src/meal.js
@@ -10,6 +10,13 @@ function createMenuItem(name, price, type) {
   }
 }
 
+
+/* The function addIngredients takes two parameters, a food string and an ingredients array;
+    If the array ingredients includes the food string,
+      return the ingredients array;
+    Otherwise, if the ingredients array does not include the food string,
+      push the food string into the ingredients array, then return the ingredients array;
+*/
 function addIngredients(food, ingredients) {
   if (ingredients.includes(food)) {
     return ingredients;
@@ -19,14 +26,24 @@ function addIngredients(food, ingredients) {
   return ingredients;
 }
 
+// The formatPrice function takes one parameter (initialPrice), an integer.
+  // return parameter (initialPrice) with a dollar sign in front of parameter value
 function formatPrice(initialPrice) {
   return `$${initialPrice}`
 }
 
+// The decreasePrice function takes one parameter (discountPrice), an integer.
+  // and returns the discountPrice parameter minus 10%
 function decreasePrice(discountPrice) {
   return discountPrice*.9;
 }
 
+
+/*  The createRecipe function takes 3 parameters, a title (string), ingredients (array), menuItemType (string);
+    and returns an object named recipe with 3 key value pairs.
+    Each value for the keys will be passed as an argument when function is invoked.
+    Then the function returns the recipe object. 
+*/
 function createRecipe(title, ingredients, menuItemType) {
   recipe = {
     title: title,
