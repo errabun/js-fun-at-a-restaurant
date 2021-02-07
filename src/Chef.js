bf5880f5214524184e75a3d88157375661421dd6
Eric Rabun <errabun@gmail.com>
2021-02-07 12:31:18 -0700

    1st chef.js attempt commit

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,22 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+  greetCustomer(customerName, bool) {
+    if (customerName && bool) {
+      return `Good morning, ${customerName}!`;
+    } else {
+      return `Hello, ${customerName}!`
+    }
+  }
+
+  // So close with this function...can't figure out why it's returning undefined
+  checkForFood(foodItem) {
+    console.log(this.restaurant.menus[foodItem.type]);
+    if (this.restaurant.menus[foodItem.type].includes(foodItem.name)) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    }
   }
 }
 
